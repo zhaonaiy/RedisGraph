4bc01e6494aa21c1835b12c398fd33744a118c43
DvirDukhan <dvir@redislabs.com>
2020-10-12 09:02:46 +0300

    wip

--- src/execution_plan/optimizations/traverse_order.h
+++ src/execution_plan/optimizations/traverse_order.h
@@ -10,24 +10,6 @@
 #include "../../filter_tree/filter_tree.h"
 #include "../../arithmetic/algebraic_expression.h"
 
-typedef enum {
-	TRAVERSE_ORDER_FIRST,
-	TRAVERSE_ORDER_LAST,
-} TRAVERSE_ORDER;
-
-// TODO comment
-typedef struct {
-	QueryGraph *qg;
-	rax *filtered_entities;
-	rax *bound_vars;
-	AlgebraicExpression **best_arrangement;
-	int max_score;
-} OrderScoreCtx;
-
-// TODO remove functions that don't need to be exposed, add comments for those that do.
-bool valid_position(AlgebraicExpression **exps, int pos, AlgebraicExpression *exp, QueryGraph *qg);
-int score_arrangement(OrderScoreCtx *score_ctx, AlgebraicExpression **arrangement, uint exp_count);
-
 /* Reorders exps such that exp[i] is the ith expression to evaluate. */
 void orderExpressions(
 	const QueryGraph *qg,           // QueryGraph containing expression entity data.
